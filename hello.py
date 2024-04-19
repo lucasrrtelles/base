8be7822eeb7f4b06f8c4b1ab161698a4c25c0718
lucas telles <lucas.telles031100@gmail.com>
2024-04-19 11:34:53 -0300

    hello multi linguas e adicionado gitignore

--- hello.py
+++ hello.py
@@ -1,5 +1,36 @@
 #!/home/linuxbrew/.linuxbrew/bin/python3
+"""Hello word Multi Linguas.
 
-#imprimir o codigo hello word
+Depedendo do ambiente configuarar a linguagem.
 
-print("Hello, word!")
+Como usar:
+Tenha a variável LANG configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ ="0.0.1"
+__author__ ="Lucas Telles"
+__license__ ="Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, Word!"
+
+if current_language =="pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language =="it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, MUndo!"
+elif current_language =="fr_FR":
+    msg = "Bonhour Monde"
+    
+print(msg)
